9b95b4d3b68e0ac20c648c57f14d997ce5de72b8
Anuj kumar sharma <141046905+anuj308@users.noreply.github.com>
2023-11-14 23:09:23 +0530

    footer

--- app.js
+++ app.js
@@ -18,3 +18,13 @@
 // });
 
 
+
+function contact(){
+    document.getElementById("footer-content").innerHTML="<p> Contact:-  newsera@gmail.com </p>"
+}
+function Developers(){
+    document.getElementById("footer-content").innerHTML=`<ul>
+    <li><span>Ankit Kumar</span>  &nbsp ankiiiiitx@gmail.com</li>
+    <li><span>Rajababu Kumar</span></li>
+    <li><span>Anuj Kumar Sharma</span></li></ul>`
+}
