5b1e0f1ba641489a1bc74c2c0a2449bf94bdbf4e
Anuj kumar sharma <141046905+anuj308@users.noreply.github.com>
2023-11-17 19:41:41 +0530

    footer

--- news era with api/pages/login/era.js
+++ news era with api/pages/login/era.js
@@ -1,9 +1,7 @@
 function about(){
     document.getElementById("footer-content").innerHTML=` <p>News era is dedicated to providing you quick and efficient news from all over the world . <br> We provide best news on our news platform.lets learn and grow together</p>`
 }
-function contact(){
-    document.getElementById("footer-content").innerHTML="<p> Contact:-  newsera@gmail.com </p>"
-}
+
 function Developers(){
     document.getElementById("footer-content").innerHTML=`<ul>
     <li><span>Ankit Kumar</span>  &nbsp ankiiiiitx@gmail.com</li>
